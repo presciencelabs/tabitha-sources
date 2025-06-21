3f62c42cce7a5bf7fe25cc51f77894c8010c0632
billy clark <billy.clark@presciencelabs.org>
2025-06-21 14:24:49 -0400

    dep updates
    minor updates
    major updates to vite/svelte/wrangler
    
    also forced to update many scripts and configs in accordance with major updates

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,15 @@
 import { sveltekit } from '@sveltejs/kit/vite'
+import tailwindcss from '@tailwindcss/vite'
 import { defineConfig } from 'vite'
 
 export default defineConfig({
 	plugins: [
+		tailwindcss(),
 		sveltekit(),
 	],
 
 	server: {
 		host: 'localhost.tbta.bible',
+		port: 8789,
 	},
 })
