e6158f3e7dbcb72e26c5e8da2a27d64420592131
craigp_atw <craig_palmer@alltheword.org>
2026-03-27 11:54:32 -0700

    Add status note or warning

--- src/routes/[type]/[id_primary]/[id_secondary]/[id_tertiary]/+page.svelte
+++ src/routes/[type]/[id_primary]/[id_secondary]/[id_tertiary]/+page.svelte
@@ -2,6 +2,7 @@
 	import { Navigation, SourceEntities } from '$lib'
 	import Settings from '$lib/settings/Settings.svelte'
 	import Sidebar from '$lib/sidebar/Sidebar.svelte'
+	import Icon from '@iconify/svelte'
 
 	/** @type {PageData} */
 	export let data
@@ -34,6 +35,22 @@
 	</div>
 </div>
 
+{#if source.status !== 'Ready to Translate' && source.parsed_semantic_encoding.length > 0}
+	{@const time_note = source.status === 'Final Review in Progress' ? 'almost' : 'not yet'}
+	<div role="alert" class="alert alert-warning">
+		<Icon icon="mdi:alert-outline" class="h-6 w-6" />
+		<span>Source data for this verse is still being reviewed, and is {time_note} ready for translation.</span>
+	</div>
+{:else if source.status === 'Not Started'}
+	<div class="flex justify-center prose max-w-full">
+		<p>No source data yet for this verse.</p>
+	</div>
+{:else if source.status !== 'Ready to Translate'}
+	<div class="flex justify-center prose max-w-full">
+		<p>Source data for this verse is currently being developed.</p>
+	</div>
+{/if}
+
 {#if source.parsed_semantic_encoding.length > 0}
 	<div class="flex h-screen">
 		<div class="transition-all duration-300 flex-[1_1_auto]" style="margin-right: {sidebar_open ? '24rem' : '0'};">
@@ -42,9 +59,5 @@
 		{#if sidebar_open}
 			<Sidebar {selected_entity} is_open={sidebar_open} {close_sidebar} noun_list={source.noun_list} />
 		{/if}
-	</div>
-{:else}
-	<div class="flex justify-center prose max-w-full">
-		<p>No source data yet for this verse.</p>
 	</div>
 {/if}
