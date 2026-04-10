afdd16b4ae9a85309d6dbb9c5814cf94f75a075f
craigp_atw <craig_palmer@alltheword.org>
2026-04-10 13:13:45 -0700

    Fix type discrepency issue

--- src/lib/data/status.ts
+++ src/lib/data/status.ts
@@ -11,7 +11,10 @@ export async function get_verse_statuses(db: D1Database, references: Reference[]
 	`
 
 	const prepared_statement = db.prepare(sql)
-	const bound_statements = references.map(({ type, id_primary, id_secondary, id_tertiary }) => prepared_statement.bind(type, id_primary, id_secondary, id_tertiary))
+	const bound_statements = references.map(({ type, id_primary, id_secondary, id_tertiary }) =>
+		prepared_statement.bind(type, id_primary, id_secondary.toString(), id_tertiary.toString()),
+	)
+
 	const batch_result = await db.batch<{ status: SourceStatus }>(bound_statements)
 	const statuses = batch_result.map(r => r.results[0]?.status || 'Not Started')
 
