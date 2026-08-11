535cc42faf17ed9fb57808fef09c3719ce9db1b4
billy clark <billy.clark@presciencelabs.org>
2026-08-11 17:34:01 -0400

    typical convention

--- src/lib/sidebar/Sidebar.svelte
+++ src/lib/sidebar/Sidebar.svelte
@@ -15,6 +15,8 @@
 	let { entity, onclose, noun_list }: Props = $props()
 </script>
 
+<svelte:window onkeydown={e => e.key === 'Escape' && onclose()} />
+
 <div class="fixed top-0 right-0 h-full w-96 bg-base-100 border-l border-base-300 shadow-xl flex flex-col">
 	<button class="btn btn-circle btn-ghost btn-sm absolute right-3 top-5" onclick={onclose} >
 		<Icon icon="material-symbols:close" class="h-4 w-4" />
